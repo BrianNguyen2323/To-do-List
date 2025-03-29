8edf22cf47ead8ca36d5d73a01cbcca887592c0c
Brian Nguyen <nguyenbrian2323@gmail.com>
2025-03-28 17:10:47 -0700

    basic UI

---
+++ src/components/ui/notepad.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+function Notepad() {
+  return (
+    <div className='h-screen rounded-lg w-full border-2 border-zinc-400 bg-zinc-800 backdrop-blur-sm'></div>
+  );
+}
+
+export { Notepad };
