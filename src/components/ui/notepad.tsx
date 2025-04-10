578cd74778d868f67e4907e1e7c23024813cdc41
Brian Nguyen <nguyenbrian2323@gmail.com>
2025-04-10 10:57:24 -0700

    components for tasks in progress

--- src/components/ui/notepad.tsx
+++ src/components/ui/notepad.tsx
@@ -1,8 +1,16 @@
 'use client';
 
+import { Task } from '@/components/ui/task';
+
 function Notepad() {
   return (
-    <div className='h-screen rounded-lg w-full border-2 border-zinc-400 bg-zinc-800 backdrop-blur-sm'></div>
+    <div className='h-screen rounded-lg w-full border-2 border-zinc-400 bg-zinc-800 backdrop-blur-sm'>
+      <ul className='w-full'>
+        <Task />
+        <Task />
+        <Task />
+      </ul>
+    </div>
   );
 }
 
