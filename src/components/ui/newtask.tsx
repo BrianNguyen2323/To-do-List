44a4d458b7a8a7a3148e5f3b39d217e322c64d1b
Brian <nguyenbrian2323@gmail.com>
2025-04-11 16:20:30 -0700

    initial UI

---
+++ src/components/ui/newtask.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+
+const Newtask = () => {
+  return (
+    <div className='flex justify-center'>
+      <Button className='w-3/4'> + New Task </Button>
+    </div>
+  );
+};
+
+export { Newtask };
--- src/components/ui/notepad.tsx
+++ src/components/ui/notepad.tsx
@@ -1,12 +1,14 @@
 'use client';
 
 import { Task } from '@/components/ui/task';
+import { Newtask } from '@/components/ui/newtask';
 
 function Notepad() {
   return (
     <div className='h-screen rounded-lg w-full border-2 border-zinc-400 bg-zinc-800 backdrop-blur-sm'>
       <ul className='w-full'>
         <Task />
+        <Newtask />
       </ul>
     </div>
   );
