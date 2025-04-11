c7ac085b59ed3b5045b1de02430e59248649830e
Brian <nguyenbrian2323@gmail.com>
2025-04-11 16:21:24 -0700

    saved changes

--- src/components/ui/newtask.tsx
+++ src/components/ui/newtask.tsx
@@ -4,7 +4,7 @@ import { Button } from '@/components/ui/button';
 
 const Newtask = () => {
   return (
-    <div className='flex justify-center'>
+    <div className='flex justify-center '>
       <Button className='w-3/4'> + New Task </Button>
     </div>
   );
